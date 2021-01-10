11c89df6da29f9dc3b307a8dba41cb2e5b75dfb9
choisuji <choisuji0217@gmail.com>
2021-01-10 19:17:09 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import { Divider } from 'antd';
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
-function App() {
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Divider>😂😈😀😎</Divider>
+      <p>
+        리액트&& 타입스크립트 프로젝트 
+      </p>
+      <Divider orientation="left">Left Text</Divider>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider orientation="right">Right Text</Divider>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+      <Divider>😂😈😀😎</Divider>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={ {
+        
+        }}
+      />
     </div>
   );
 }
 
 export default App;
+//https://finance.naver.com/item/fchart.nhn?code=005930
+//시가 https://finance.naver.com/item/item_right_ajax.nhn?type=recent&code=005930&page=1
+//차트 https://fchart.stock.naver.com/siseJson.nhn?symbol=005930&requestType=1&startTime=20200416&endTime=20210109&timeframe=day
+
+
+
+//https://react.vlpt.us/using-typescript/02-ts-react-basic.html
+
